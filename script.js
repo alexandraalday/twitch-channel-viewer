3781778a18b35253a4482969f53931c99973a3ce
alexandraalday <alexandra.alday@gmail.com>
2017-10-31 15:31:27 -0700

    first pass at JSON data. needs refactoring asap

--- script.js
+++ script.js
@@ -14,13 +14,34 @@ $(document).ready(function(){
 		following.push('ESL_SC2');
 
 		for (let i=0; i< following.length; i++){
-			let url = proxyUrl + 'https://wind-bow.gomix.me/twitch-api/streams/' + following[i] + '/?callback=?';
-			$.getJSON(url).done(function(data2){
+			let url = 'https://wind-bow.gomix.me/twitch-api/streams/' + following[i] + '/?callback=?';
+			$.getJSON(proxyUrl + url).done(function(data2){
 				let logo;
 				let status;
 				let name;
 				if (data2.error){
 					console.log(data2.error);
+					name = data2.message;
+					status = data2.error;
+
+					$('#followerInfo').prepend("<div class='row'>" + 
+						"<div class='col-md-4'>" + "<img src='" + logo + "'>" + "</div>" + 
+						"<div class='col-md-4'>" + name + "</div>" +
+						"<div class='col-md-4'>" + status + "</div></div>")
+				}
+			});
+		}
+		for (let i=0; i< following.length; i++){
+			let onlineUrl = 'https://wind-bow.gomix.me/twitch-api/streams/' + following[i];
+			$.getJSON(proxyUrl + onlineUrl, function(data3){
+					logo = data3.stream.channel.logo;
+					name = data3.stream.channel.name;
+					status = data3.stream.channel.status;
+
+					$('#followerInfo').prepend("<div class='row'>" + 
+						"<div class='col-md-4'>" + "<img src='" + logo + "'>" + "</div>" + 
+						"<div class='col-md-4'>" + name + "</div>" +
+						"<div class='col-md-4'>" + status + "</div></div>")
 				}
 			});
 		}
